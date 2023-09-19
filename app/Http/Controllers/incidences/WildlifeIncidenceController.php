7df103634384038ae7dffa5acac0cdc3c3e784ee
dohd <alloicebrian@gmail.com>
2023-09-19 10:19:32 +0300

    Update wildlife store and update methods

--- app/Http/Controllers/incidences/WildlifeIncidenceController.php
+++ app/Http/Controllers/incidences/WildlifeIncidenceController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\incidences;
 use App\Http\Controllers\Controller;
 use App\Models\IncidentFile;
 use App\Models\Region;
-use App\Models\WidlifeIncidence;
+use App\Models\WildlifeIncidence;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -23,7 +23,7 @@ class WildlifeIncidenceController extends Controller
     public function index()
     {
         if (request()->ajax()) {
-            $data = WidlifeIncidence::orderBy('created_at', 'DESC')->get();
+            $data = WildlifeIncidence::orderBy('created_at', 'DESC')->get();
             return DataTables::of($data)
                 ->addColumn('date_commited', function ($data) {
                     return  dateFormat($data->date_commited);
@@ -58,77 +58,66 @@ class WildlifeIncidenceController extends Controller
      */
     public function store(Request $request)
     {
-        if (request()->ajax()) {
-            $input = $request->except(['_token']);
+        if (!request()->ajax()) return redirect()->back();
 
-            $validator = Validator::make($request->all(), [
-                'date_commited' => 'required',
-            
-            ]);
+        $input = $request->except(['_token']);
+        $validator = Validator::make($request->all(), [
+            'date_commited' => 'required',
+        ]);
 
-            $noerrors = true;
-            if ($validator->fails()) {
-                $output = [
-                    'success' => false,
-                    'msg' => "Date field is required",
-                ];
-                $noerrors = false;
-            }
-            $input['date_commited']=date_for_database($input['date_commited']);
-            $inputDateCarbon = Carbon::parse($input['date_commited']);
-            $month = $inputDateCarbon->month;
-            $year = $inputDateCarbon->year;
-            
-            $recordsExist=WidlifeIncidence::whereYear('date_commited', $year)
-            ->whereMonth('date_commited', $month)->exists();
-            if($recordsExist){
-                $output = [
-                    'success' => false,
-                    'msg' => "Record for this day is already captured!!",
-                ];
-                $noerrors = false;
-            }
-
-            if ($noerrors) {
-                try {
-                    //Begin DB  
-                    DB::beginTransaction();
-                    $input['user_id']=auth()->user()->id;
-                    $result = WidlifeIncidence::create($input);
-                    $amaster=[];
-                    foreach($input['incident_file_id'] as $key=>$value){
-
-                        $amaster[]=array(
-                            'statistic_value'=>form_return($input['statistic_value'][$key]),
-                            'incident_file_id'=>$value,
-                            'region_id'=>$input['region_id'][$key],
-                        );
-
-                    }
-                   
-                   $result->wildlifeStastics()->createMany($amaster);
-                  
-                
-                    DB::commit();
-                    $output = [
-                        'success' => true,
-                        'msg' => "Record Saved Successfully",
-                    ];
-
-                } catch (\Exception $e) {
-                    DB::rollBack();
-                    Log::emergency("File:" . $e->getFile() . "Line:" . $e->getLine() . "Message:" . $e->getMessage());
-                    $output = [
-                        'success' => false,
-                        'msg' => $e->getMessage(),
-                    ];
-                }
-
-            }
-          
-        return $output;
-      
+        if ($validator->fails()) {
+            return [
+                'success' => false,
+                'msg' => "Date field is required",
+            ];
         }
+        $input['date_commited'] = date_for_database($input['date_commited']);
+        $inputDateCarbon = Carbon::parse($input['date_commited']);
+        $month = $inputDateCarbon->month;
+        $year = $inputDateCarbon->year;
+        
+        $exists = WildlifeIncidence::whereYear('date_commited', $year)
+            ->whereMonth('date_commited', $month)->exists();
+        if($exists) {
+            return [
+                'success' => false,
+                'msg' => "Record for this day is already captured!!",
+            ];
+        }
+
+        try {
+            //Begin DB  
+            DB::beginTransaction();
+
+            $input['user_id'] = auth()->user()->id;
+            $result = WildlifeIncidence::create($input);
+
+            $amaster = [];
+            foreach($input['incident_file_id'] as $key=>$value){
+                $amaster[] = [
+                    'statistic_value'=>form_return($input['statistic_value'][$key]),
+                    'incident_file_id'=>$value,
+                    'region_id'=>$input['region_id'][$key],
+                ];
+            }
+            $amaster = array_filter($amaster, fn($v) => @$v['statistic_value']);
+            if (!$amaster) trigger_error('Wildlife statistic required');
+            $result->wildlifeStastics()->createMany($amaster);
+            
+            DB::commit();
+            $output = [
+                'success' => true,
+                'msg' => "Record Saved Successfully",
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::emergency("File:" . $e->getFile() . "Line:" . $e->getLine() . "Message:" . $e->getMessage());
+            $output = [
+                'success' => false,
+                'msg' => $e->getMessage(),
+            ];
+        }
+        return $output;
     }
 
     /**
@@ -152,7 +141,7 @@ class WildlifeIncidenceController extends Controller
     {
         $incidences=IncidentFile::where('crime_category_id',23)->get();
         $regions=Region::where('is_report',1)->get();
-        $wildlifeincidence = WidlifeIncidence::with(['wildlifeStastics'])->where('uuid', $id)->first();
+        $wildlifeincidence = WildlifeIncidence::with(['wildlifeStastics'])->where('uuid', $id)->first();
       return view('wildlife.edit',compact('wildlifeincidence','incidences','regions'));
     }
 
@@ -165,68 +154,57 @@ class WildlifeIncidenceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if (request()->ajax()) {
-            $input_main = $request->only(['date_commited','elephant','rhino','giraffe','injured','fetal']);
+        if (!request()->ajax()) return redirect()->back();
 
-            $validator = Validator::make($request->all(), [
-                'date_commited' => 'required',
-            
-            ]);
+        $input_main = $request->only(['date_commited','elephant','rhino','giraffe','injured','fetal']);
+        $validator = Validator::make($request->all(), [
+            'date_commited' => 'required',
+        ]);
 
-            $noerrors = true;
-            if ($validator->fails()) {
-                $output = [
-                    'success' => false,
-                    'msg' => "Date field is required",
-                ];
-                $noerrors = false;
-            }
-            $input_main['date_commited']=date_for_database($input_main['date_commited']);
-        
-            if ($noerrors) {
-                try {
-                    //Begin DB  
-                    DB::beginTransaction();
-                    $input_main['user_id']=auth()->user()->id;
-                    $result = WidlifeIncidence::find($id);
-                    $result->update($input_main);
-                    $result->touch();
-                    $input = $request->except(['_token']);
-                    $result->wildlifeStastics()->delete();
-                    $amaster=[];
-                    foreach($input['incident_file_id'] as $key=>$value){
-
-                        $amaster[]=array(
-                            'statistic_value'=>form_return($input['statistic_value'][$key]),
-                            'incident_file_id'=>$value,
-                            'region_id'=>$input['region_id'][$key],
-                        );
-
-                    }
-                   
-                   $result->wildlifeStastics()->createMany($amaster);
-                  
-                
-                    DB::commit();
-                    $output = [
-                        'success' => true,
-                        'msg' => "Record Updated Successfully",
-                    ];
-
-                } catch (\Exception $e) {
-                    DB::rollBack();
-                    Log::emergency("File:" . $e->getFile() . "Line:" . $e->getLine() . "Message:" . $e->getMessage());
-                    $output = [
-                        'success' => false,
-                        'msg' => $e->getMessage(),
-                    ];
-                }
-
-            }
-          
-        return $output;
-      
+        if ($validator->fails()) {
+            return [
+                'success' => false,
+                'msg' => "Date field is required",
+            ];
         }
+        $input_main['date_commited']=date_for_database($input_main['date_commited']);
+    
+        try {
+            //Begin DB  
+            DB::beginTransaction();
+
+            $input_main['user_id'] = auth()->user()->id;
+            $result = WildlifeIncidence::find($id);
+            $result->update($input_main);
+            $result->touch();
+            $input = $request->except(['_token']);
+            $result->wildlifeStastics()->delete();
+            $amaster=[];
+            foreach($input['incident_file_id'] as $key=>$value){
+                $amaster[] = array(
+                    'statistic_value'=>form_return($input['statistic_value'][$key]),
+                    'incident_file_id'=>$value,
+                    'region_id'=>$input['region_id'][$key],
+                );
+            }
+            $amaster = array_filter($amaster, fn($v) => @$v['statistic_value']);
+            if (!$amaster) trigger_error('Wildlife statistic required');
+            $result->wildlifeStastics()->createMany($amaster);
+            
+            DB::commit();
+            $output = [
+                'success' => true,
+                'msg' => "Record Updated Successfully",
+            ];
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::emergency("File:" . $e->getFile() . "Line:" . $e->getLine() . "Message:" . $e->getMessage());
+            $output = [
+                'success' => false,
+                'msg' => $e->getMessage(),
+            ];
+        }
+        return $output;
     }
 
     /**
