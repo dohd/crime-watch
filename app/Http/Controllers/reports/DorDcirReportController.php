4f1c1c70e3fcdd140dcfb2fcba5796a1654b05da
dohd <alloicebrian@gmail.com>
2023-10-06 09:59:48 +0300

    Format document

--- app/Http/Controllers/reports/DorDcirReportController.php
+++ app/Http/Controllers/reports/DorDcirReportController.php
@@ -89,42 +89,42 @@ class DorDcirReportController extends Controller
     {
 
         $report_numbers = IncidentRecord::where('report_type', 'Briefing Report')
-        ->distinct()
-        ->orderBy('created_at', 'desc')
-        ->pluck('incident_no','incident_no');
+            ->distinct()
+            ->orderBy('created_at', 'desc')
+            ->pluck('incident_no', 'incident_no');
         //210/2023
-       
-       
 
-  
-     
-       
-       
-       
-        return view('report.dor',compact('report_numbers'));
+
+
+
+
+
+
+
+        return view('report.dor', compact('report_numbers'));
     }
 
     public function dcir_report()
     {
 
         $report_numbers = IncidentRecord::where('report_type', 'Briefing Report')
-        ->distinct()
-        ->orderBy('created_at', 'desc')
-        ->pluck('incident_no','incident_no');
-    
-     
-       
-        return view('report.dcir',compact('report_numbers'));
+            ->distinct()
+            ->orderBy('created_at', 'desc')
+            ->pluck('incident_no', 'incident_no');
+
+
+
+        return view('report.dcir', compact('report_numbers'));
     }
 
 
     public function get_dor_Report(Request $request)
     {
 
-        $report_numbers=$request->report_number;
-        $is_dcir=$request->is_dcir;
-     
-        
+        $report_numbers = $request->report_number;
+        $is_dcir = $request->is_dcir;
+
+
 
         $counties = County::with([
             'divisions' => function ($q) use ($report_numbers, $is_dcir) {
@@ -145,7 +145,7 @@ class DorDcirReportController extends Controller
             $query->where('is_dcir', $is_dcir);
         })->orderBy('code', 'desc')->get();
 
-         /* $master=[];
+        /* $master=[];
 
           foreach($counties as $county){
 
@@ -166,17 +166,18 @@ class DorDcirReportController extends Controller
             }
 
           }*/
-        
 
-        return view('report.section.dor',compact('counties','report_numbers','is_dcir'));
+
+        return view('report.section.dor', compact('counties', 'report_numbers', 'is_dcir'));
     }
+
+    /**
+     * Print DCIR Report 
+     */
     public function print_dcir_report($report_number, $is_dcir)
     {
-
-        $report_numbers=decrypt_data($report_number);
-        $is_dcir=decrypt_data($is_dcir);
-     
-        
+        $report_numbers = decrypt_data($report_number);
+        $is_dcir = decrypt_data($is_dcir);
 
         $counties = County::with([
             'divisions' => function ($q) use ($report_numbers, $is_dcir) {
@@ -197,42 +198,90 @@ class DorDcirReportController extends Controller
             $query->where('is_dcir', $is_dcir);
         })->orderBy('code', 'desc')->get();
 
-          $date=IncidentRecord::where('incident_no', $report_numbers)->where('is_dcir',$is_dcir)->first();
+        $date = IncidentRecord::where('incident_no', $report_numbers)->where('is_dcir', $is_dcir)->first();
 
-          $from_date=dorDateFormat($date->created_at);
-          $title='DAILY OPERATION REPORT';
-          if($is_dcir==1){
-            $title='DAILY CRIME AND INCIDENCE REPORT';
+        $from_date = dorDateFormat($date->created_at);
+        $title = 'DAILY OPERATION REPORT';
+        if ($is_dcir == 1) {
+            $title = 'DAILY CRIME AND INCIDENCE REPORT';
+        }
+        //dd($counties);
 
-          }
-          //dd($counties);
+        // Get your data to be used in the PDF, e.g., from the database or any other source.
+        $data = [
+            'title' =>  $title,
+            'counties' => $counties,
+            'report_numbers' => $report_numbers,
+            'from_date' => $from_date,
+            'content' => 'This is the content of the PDF.',
+        ];
+        $pdf = PDF::loadView('report.print.dor', $data, [], [
+            'title' => 'DOR REPORT',
+        ]);
+        // You can also set additional configurations for the PDF, such as page orientation, size, etc.
+        // For example:
+        // $pdf->setPaper('A4', 'landscape');
 
-         // Get your data to be used in the PDF, e.g., from the database or any other source.
-    $data = [
-        'title' =>  $title,
-        'counties'=>$counties,
-        'report_numbers'=>$report_numbers,
-        'from_date'=>$from_date,
-        'content' => 'This is the content of the PDF.',
-    ];
-    $pdf = PDF::loadView('report.print.dor', $data,[],[
-        'title'=>'DOR REPORT',
-    ]);
-    // You can also set additional configurations for the PDF, such as page orientation, size, etc.
-    // For example:
-    // $pdf->setPaper('A4', 'landscape');
+        return $pdf->stream('sample.pdf');
 
-    return $pdf->stream('sample.pdf');
-
-   // return $pdf->download('sample.pdf');
+        // return $pdf->download('sample.pdf');
 
     }
-    
 
+    /**
+     * Print DOR Report 
+     */
+    public function print_dor_report($report_number, $is_dcir)
+    {
+        $report_numbers = decrypt_data($report_number);
+        $is_dcir = decrypt_data($is_dcir);
 
-    
-    
-    
+        $counties = County::with([
+            'divisions' => function ($q) use ($report_numbers, $is_dcir) {
+                $q->whereHas('divincidences', function ($query) use ($report_numbers, $is_dcir) {
+                    $query->where('incident_no', $report_numbers);
+                    $query->where('report_type', 'Briefing Report');
+                    $query->where('is_dcir', $is_dcir);
+                });
+            },
+            'divisions.divincidences' => function ($q) use ($report_numbers, $is_dcir) {
+                $q->where('incident_no', $report_numbers);
+                $q->where('report_type', 'Briefing Report');
+                $q->where('is_dcir', $is_dcir);
+            },
+        ])->whereHas('incidences', function ($query) use ($report_numbers, $is_dcir) {
+            $query->where('incident_no', $report_numbers);
+            $query->where('report_type', 'Briefing Report');
+            $query->where('is_dcir', $is_dcir);
+        })->orderBy('code', 'desc')->get();
 
-    
+        $date = IncidentRecord::where('incident_no', $report_numbers)->where('is_dcir', $is_dcir)->first();
+
+        $from_date = dorDateFormat($date->created_at);
+        $title = 'DAILY OPERATION REPORT';
+        if ($is_dcir == 1) {
+            $title = 'DAILY CRIME AND INCIDENCE REPORT';
+        }
+        //dd($counties);
+
+        // Get your data to be used in the PDF, e.g., from the database or any other source.
+        $data = [
+            'title' =>  $title,
+            'counties' => $counties,
+            'report_numbers' => $report_numbers,
+            'from_date' => $from_date,
+            'content' => 'This is the content of the PDF.',
+        ];
+        $pdf = PDF::loadView('report.print.dor', $data, [], [
+            'title' => 'DOR REPORT',
+        ]);
+        // You can also set additional configurations for the PDF, such as page orientation, size, etc.
+        // For example:
+        // $pdf->setPaper('A4', 'landscape');
+
+        return $pdf->stream('sample.pdf');
+
+        // return $pdf->download('sample.pdf');
+
+    }
 }
